f6f8e91f8a9cd1a0d626a94724fb0df47169cd24
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2020-09-21 14:17:49 +0300

    Prepare m2 for tests

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,18 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+
+# install warden
+brew install davidalger/warden/warden
+
+# clone magento 2 repo
+git clone https://github.com/magento/magento2.git
+
+cd magento2;
+
+#init project
+warden env-init exampleproject magento2
+
+# enable test db
+sed  -i -e 's/WARDEN_TEST_DB=0/WARDEN_TEST_DB=1/g' .env
+
+# start warden
+warden env up
