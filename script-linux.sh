b9ce99748a584754f33acd855736968110c1a994
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2020-09-21 14:23:40 +0300

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -16,3 +16,40 @@ sed  -i -e 's/WARDEN_TEST_DB=0/WARDEN_TEST_DB=1/g' .env
 
 # start warden
 warden env up
+
+# prepare configs
+cat - <<EOF | tee dev/tests/integration/etc/install-config-mysql.php
+<?php
+return [
+    'db-host' => 'tmp-mysql',
+    'db-user' => 'root',
+    'db-password' => 'magento',
+    'db-name' => 'magento_integration_tests',
+    'backend-frontname' => 'backend',
+    'search-engine' => 'elasticsearch7',
+    'elasticsearch-host' => 'elasticsearch',
+    'elasticsearch-port' => 9200,
+    'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
+    'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
+    'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
+    'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
+    'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
+    'amqp-host' => 'rabbitmq',
+    'amqp-port' => '5672',
+    'amqp-user' => 'guest',
+    'amqp-password' => 'guest',
+    'session-save' => 'redis',
+    'session-save-redis-host' => 'redis',
+    'session-save-redis-port' => 6379,
+    'session-save-redis-db' => 2,
+    'session-save-redis-max-concurrency' => 20,
+    'cache-backend' => 'redis',
+    'cache-backend-redis-server' => 'redis',
+    'cache-backend-redis-db' => 0,
+    'cache-backend-redis-port' => 6379,
+    'page-cache' => 'redis',
+    'page-cache-redis-server' => 'redis',
+    'page-cache-redis-db' => 1,
+    'page-cache-redis-port' => 6379,
+];
+EOF
